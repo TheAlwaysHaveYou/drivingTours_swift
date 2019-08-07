e905ae8de4159d455701670f722a81b5cb1aa1ff
范魁东 <fankuidong@fankuidongdeMacBook-Pro.local>
2019-08-07 11:39:00 +0800

    测试

--- driving_tour_swift/AppDelegate.swift
+++ driving_tour_swift/AppDelegate.swift
@@ -13,7 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
+    //测试分支
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         self.window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
